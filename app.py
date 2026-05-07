b51eb8a2682c5294cf79f051f441d36fc775eea7
Magnin1872888199 <gabrielwillyam028@gmail.com>
2026-05-07 14:51:45 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+whitelist = {"users": ["joao", "maria"]}
+
+@app.route("/get", methods=["GET"])
+def get_whitelist():
+    return jsonify(whitelist)
+
+@app.route("/update", methods=["POST"])
+def update_whitelist():
+    global whitelist
+    whitelist = request.json
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
