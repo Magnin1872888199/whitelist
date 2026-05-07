8fd268ad2bdd7593f005663263d490baffeef550
Magnin1872888199 <gabrielwillyam028@gmail.com>
2026-05-07 15:11:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 whitelist = {"users": ["joao", "maria"]}
 
 @app.route("/get", methods=["GET"])
-def get_whitelist():
+def get():
     return jsonify(whitelist)
 
 @app.route("/update", methods=["POST"])
-def update_whitelist():
+def update():
     global whitelist
     whitelist = request.json
     return {"status": "ok"}
